c5b1e17af55b020b8249af8fe43757702ca14298
Vizzyy <barnabas.vizy@gmail.com>
2020-05-05 13:48:13 -0400

    Implement uploading video files as gifs utilizing gif conversion via gfycat

---
+++ upload_video.py
@@ -0,0 +1,50 @@
+import time
+import requests
+
+
+def convert_to_gif(local_filename):
+    try:
+        # local_filename = "IMG_0012.MOV"  # sys.argv[1]
+
+        files = {'file': open(local_filename, 'rb')}
+
+        # curl -v -XPOST https://api.gfycat.com/v1/gfycats -H "Content-Type: application/json"
+
+        headers = {'Content-Type': 'application/json'}
+        response_initial = requests.post("https://api.gfycat.com/v1/gfycats", headers=headers)
+        print(response_initial.json())
+        gfyname = response_initial.json()['gfyname']
+
+        # curl -i https://filedrop.gfycat.com --upload-file ./gfyname
+
+        payload = {'key': gfyname}
+        response_upload = requests.post("https://filedrop.gfycat.com", files=files, data=payload)
+        print("file uploaded!")
+        print(response_upload.text)
+        time.sleep(2)
+
+        # GET https://api.gfycat.com/v1/gfycats/fetch/status/gfyname
+        # {'task': 'complete', 'gfyname': 'gfyname'}
+        gif_link = ""
+        while True:
+            response_status = requests.get(f"https://api.gfycat.com/v1/gfycats/fetch/status/{gfyname}")
+            print(response_status.json())
+            upload_status = response_status.json()['task']
+            if upload_status == "complete":
+                break
+            time.sleep(5)
+
+        # /v1/me/gfycats/{gfyId}/published	PUT	Update/replace the gfycat published. Params: 'value’, Values: '0’,'1’
+
+        print("Downloading GIF...")
+        uri = f'https://thumbs.gfycat.com/{gfyname}-size_restricted.gif'
+        with open(f'{local_filename[:-4]}.gif', 'wb') as f:
+            f.write(requests.get(uri).content)
+            print("Finished download!")
+
+        local_gif = f'{local_filename[:-4]}.gif'
+        return local_gif
+
+    except Exception as e:
+        print(e)
+        return None
--- uploader.py
+++ uploader.py
@@ -2,11 +2,11 @@ import glob
 import os
 import sys
 import shutil
-import time
 from datetime import datetime
 from imgurpython import ImgurClient
 from imgurpython.helpers.error import ImgurClientError
 from config import *
+from upload_video import *
 
 client_id = get_client_id()
 client_secret = get_client_secret()
@@ -16,11 +16,12 @@ username = get_username()
 
 # seconds to wait between uploads
 # otherwise api rate-limiting fault
-sleep_length = 75
-
+sleep_length = 5
+now = datetime.now()
+print(f"{now} - Script started.\n")
 client = ImgurClient(client_id, client_secret, access_token, refresh_token)
 
-today = str(datetime.date(datetime.now()))
+today = str(datetime.date(now))
 
 try:
     # check credit limit, if none exit
@@ -55,6 +56,16 @@ try:
 
     # glob files in directory
     fullList = glob.glob('*.jpg')
+    fullList += glob.glob('*.JPG')
+    fullList += glob.glob('*.PNG')
+    fullList += glob.glob('*.png')
+    fullList += glob.glob('*.GIF')
+    fullList += glob.glob('*.gif')
+    fullList += glob.glob('*.MOV')
+    fullList += glob.glob('*.mov')
+    fullList += glob.glob('*.MP4')
+    fullList += glob.glob('*.mp4')
+
     list.sort(fullList)
     print(f"There are {len(fullList)} images to parse.")
 
@@ -62,11 +73,22 @@ try:
     image_config = {'album': album_id}
     for image in fullList:
         now = datetime.now()
-        print(f"{now} - Attempting to upload {image} ...")
-        response = client.upload_from_path(image, image_config, False)
+        file_extension = image[-4:].lower()
+        print(f"{now} - Attempting to upload {image}, with file extension '{file_extension}'...")
+        final_path = ""
+
+        if file_extension == '.mp4' or file_extension == '.mov':
+            print(f"Found video {image}; attempting to convert...")
+            final_path = convert_to_gif(image)
+            print(f"Gif name: {final_path}")
+            shutil.move(image, "./finished_uploads/")
+        else:
+            final_path = image
+
+        response = client.upload_from_path(final_path, image_config, False)
         print(response)
         print("Now moving file...")
-        shutil.move(image, "./finished_uploads/")
+        shutil.move(final_path, "./finished_uploads/")
         time.sleep(sleep_length)
 
 except ImgurClientError as e:
@@ -75,6 +97,5 @@ except ImgurClientError as e:
 except Exception as e:
     print(e)
 
-print(ImgurClient.get_credits(client))
 now = datetime.now()
-print(f"{now} - Script finished.")
+print(f"\n{now} - Script finished.\n")
